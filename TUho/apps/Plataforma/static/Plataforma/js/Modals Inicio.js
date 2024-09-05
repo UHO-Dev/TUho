b5e4bbbb3e068aa3d4820bcf17dc9c0d985f54c2
RobertoGP96 <trolbertogp96@gmail.com>
2024-09-05 09:59:57 -0400

    rebuild project

--- TUho/apps/Plataforma/static/Plataforma/js/Modals Inicio.js
+++
@@ -1,9 +0,0 @@
-var Inicio = document.getElementById('Inicio')
-Inicio.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
-  var button = event.relatedTarget
-  // Extract info from data-bs-* attributes
-  var recipient = button.getAttribute('data-bs-whatever')
-  
-})
-
